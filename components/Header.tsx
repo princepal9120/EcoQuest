cb9f3bcb55ddc648e1745942a9b413586171ab4f
Prince Pal <princepal9120@gmail.com>
2024-09-15 02:20:12 +0530

    create Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,61 +1,61 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-// import { Web3Auth } from "@web3auth/modal";
-// import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
-// import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
-// import { usePathname } from "next/navigation";
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { usePathname } from "next/navigation";
 
-// const clientId = process.env.WEB3_AUTH_CLIENT_ID;
+const clientId = process.env.WEB3_AUTH_CLIENT_ID;
 
-// const chainConfig = {
-//   chainNamespace: CHAIN_NAMESPACES.EIP155,
-//   chainId: "0xa36a7",
-//   rpcTarget: "https://rpc.ankr.com/eth_sepolia",
-//   displayName: "Sepolia Testget",
-//   blockExploreUrl: "https://sepolia.etherscan.io",
-//   ticker: "ETH",
-//   tickerName: "Ethereum",
-//   logo: "https://assets.web3auth.io/evm-chains/sepolia.png",
-// };
-// const privateKeyProvider = new EthereumPrivateKeyProvider({
-//   config: chainConfig,
-// });
+const chainConfig = {
+  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  chainId: "0xa36a7",
+  rpcTarget: "https://rpc.ankr.com/eth_sepolia",
+  displayName: "Sepolia Testget",
+  blockExploreUrl: "https://sepolia.etherscan.io",
+  ticker: "ETH",
+  tickerName: "Ethereum",
+  logo: "https://assets.web3auth.io/evm-chains/sepolia.png",
+};
+const privateKeyProvider = new EthereumPrivateKeyProvider({
+  config: chainConfig,
+});
 
-// const web3auth = new Web3Auth({
-//   clientId,
-//   web3AuthNetwork: WEB3AUTH_NETWORK.TESTNET,
-//   privateKeyProvider,
-// });
-// interface HeaderProps {
-//   onMenuClick: () => void;
-//   totalEarnings: number;
-// }
+const web3auth = new Web3Auth({
+  clientId,
+  web3AuthNetwork: WEB3AUTH_NETWORK.TESTNET,
+  privateKeyProvider,
+});
+interface HeaderProps {
+  onMenuClick: () => void;
+  totalEarnings: number;
+}
 
-// export default function Header({ onMenuClick, totalEarnings }: HeaderProps) {
-//   const [provider, setProvider] = useState<IProvider | null>(null);
-//   const [loggedIn, setLoggedIn] = useState(false);
-//   const [loading, setLoading] = useState(true);
-//   const [userInfo, setUserInfo] = useState<any>(null);
-//   const pathname = usePathname();
-//   const [notification, setNotification] = useState<Notification[]>([]);
+export default function Header({ onMenuClick, totalEarnings }: HeaderProps) {
+  const [provider, setProvider] = useState<IProvider | null>(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [userInfo, setUserInfo] = useState<any>(null);
+  const pathname = usePathname();
+  const [notification, setNotification] = useState<Notification[]>([]);
 
-//   const [balance, setBalance] = useState(0);
-//   useEffect(() => {
-//     const init = async () => {
-//       try {
-//         setProvider(web3auth.provider);
-//         if (web3auth.connected) {
-//           setLoggedIn(true);
-//           const user = await web3auth.getUserInfo();
-//           setUserInfo(user);
-//           if (user.email) {
-//             localStorage.setItem("userEmail", user.email);
-//             await createUser;
-//           }
-//         }
-//       } catch (error) {
-//         console.log("error", error);
-//       }
-//     };
-//   });
-// }
+  const [balance, setBalance] = useState(0);
+  useEffect(() => {
+    const init = async () => {
+      try {
+        setProvider(web3auth.provider);
+        if (web3auth.connected) {
+          setLoggedIn(true);
+          const user = await web3auth.getUserInfo();
+          setUserInfo(user);
+          if (user.email) {
+            localStorage.setItem("userEmail", user.email);
+            await createUser;
+          }
+        }
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+  });
+}
