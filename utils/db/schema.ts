035fe9b69aeb79d273aa951a45e5850414965104
Prince Pal <princepal9120@gmail.com>
2024-09-14 15:46:17 +0530

    created Transactions schema

--- utils/db/schema.ts
+++ utils/db/schema.ts
@@ -48,3 +48,12 @@ export const Notifications=pgTable('notifications',{
     isRead: boolean('is_read').notNull().default(false),
     createdAt: timestamp('created_at').defaultNow().notNull()
 })
+
+export const Transactions=pgTable('transactions',{
+     id: serial('id').primaryKey(),
+     userId: integer('user_id').references(()=>Users.id).notNull(),
+     type: varchar('type',{length:20}).notNull(),
+     amount: integer('amount').notNull(),
+     description: text('descrption').notNull(),
+     date: timestamp('date').defaultNow().notNull(),
+})
