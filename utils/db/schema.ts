2a2d5fbfaffd733d0b2f29d8dc10864610f24043
Prince Pal <princepal9120@gmail.com>
2024-09-14 15:16:56 +0530

    created userSchema

--- utils/db/schema.ts
+++ utils/db/schema.ts
@@ -1,9 +1,10 @@
 import {integer, varchar, pgTable, serial, text, timestamp,jsonb, boolean} from "drizzle-orm/pg-core"
 
 
-export const User=pgTable('users',{
+export const Users=pgTable('users',{
     id: serial('id').primaryKey(),
-    email: varchar('email',{length:255}).notNull().unique()
-    
+    email: varchar('email',{length:255}).notNull().unique(),
+    name : varchar('name',{length:255}).notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull()
 
 })
