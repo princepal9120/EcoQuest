de9927fadab6e41bdf1307225a89fd08065486e6
Prince Pal <princepal9120@gmail.com>
2024-09-24 00:07:45 +0530

    add animated logo in home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   const [sidebarOpen, setSidebarOpen] = React.useState(false);
   const [totalEarnings, setTotalEarnings] = useState(0);
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
 
+
+// animate debouncing logo
+
+import { Trees } from "lucide-react";
+
+function AnimatedGlobe(){
+  return(
+    <div className="relative w-32 h-32 mx-auto mb-8">
+      <div className="absolute inset-0 rounded-full bg-green-500 opacity-20 animate-pulse"></div>
+      <div className="absolute inset-2 rounded-full bg-green-400 opacity-40 animate-ping"></div>
+      <div className="absolute inset-4 rounded-full bg-green-300 opacity-60 animate-spin"></div>
+      <div className="absolute inset-6 rounded-full bg-green-200 opacity-680 animate-bounce"></div>
+     <Trees className="absolute inset-0 h-16 w-16 m-auto text-green-600 animate-pulse"/>
+    </div>
+
+  )
+}
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-   <h1>hello</h1>
+    <div className="container mx-auto px-4 py-16">
+      <section className="text-center mb-20">
+        <AnimatedGlobe/>
+
+
+      </section>
+ 
     </div>
   );
 }
